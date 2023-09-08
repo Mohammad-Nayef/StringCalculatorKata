d2c9eb9cc49e69b0dc379d400eb9dacb59d9c0f6
mohammad-nayef <awladmohammad20@gmail.com>
2023-09-08 18:40:42 +0300

    Add() supports adding values with custom separator

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -4,7 +4,19 @@
     {
         public static int Add(string numbers)
         {
+            if (numbers.StartsWith("//"))
+                return SumOfIntegersWithCustomDelimiter(numbers);
+
             var values = numbers.Split(',', '\n');
+
+            return SumOfIntegers(values);
+        }
+
+        private static int SumOfIntegersWithCustomDelimiter(string numbers)
+        {
+            var numbersWithoutSlashes = numbers.Substring(2);
+            var delimiter = numbersWithoutSlashes.First();
+            var values = numbersWithoutSlashes.Split(',', '\n', delimiter);
 
             return SumOfIntegers(values);
         }
