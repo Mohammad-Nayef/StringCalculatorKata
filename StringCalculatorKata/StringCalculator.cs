dcb58528c182e6888b8d92b5d2ec8b9fc5813d6d
mohammad-nayef <awladmohammad20@gmail.com>
2023-09-08 15:21:39 +0300

    Created Add method to add up to 2 numbers

---
+++ StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace StringCalculatorKata
+{
+    public static class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            var values = numbers.Split(',');
+            var sum = 0;
+
+            foreach (var value in values)
+            {
+                if (int.TryParse(value, out var number))
+                {
+                    sum += number;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
