cdc06e38b9bf897ca8a42ce451c88bb7e703b32b
mohammad-nayef <awladmohammad20@gmail.com>
2023-09-08 19:13:25 +0300

    created NegativesNotAllowed exception

---
+++ StringCalculatorKata/NegativesNotAllowed.cs
@@ -0,0 +1,23 @@
+﻿namespace StringCalculatorKata
+{
+    public class NegativesNotAllowed : Exception
+    {
+        public NegativesNotAllowed(int[] negatives)
+        {
+            var errorMessage = "negatives not allowed: ";
+            AppendNegativesToErrorMessage(ref errorMessage, negatives);
+
+            throw new Exception(errorMessage);
+        }
+
+        private void AppendNegativesToErrorMessage(ref string errorMessage, int[] negatives)
+        {
+            foreach (var negative in negatives)
+            {
+                errorMessage += $"{negative}, ";
+            }
+
+            errorMessage.TrimEnd(',', ' ');
+        }
+    }
+}
