da854c054e501994c9d4d4049d8208291a978b4b
mohammad-nayef <awladmohammad20@gmail.com>
2023-09-08 20:29:16 +0300

    Add() throws NegativesNotAllowedException

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -4,36 +4,82 @@
     {
         public static int Add(string numbers)
         {
+            string[] values;
+
             if (numbers.StartsWith("//"))
-                return SumOfIntegersWithCustomDelimiter(numbers);
+            {
+                values = GetValuesWithCustomDelimiter(numbers);
+            }
+            else
+            {
+                values = numbers.Split(',', '\n');
+            }
 
-            var values = numbers.Split(',', '\n');
+            var intNumbers = GetIntegers(values);
 
-            return SumOfIntegers(values);
+            if (HasNegatives(intNumbers))
+            {
+                throw new NegativesNotAllowed(GetNegatives(intNumbers));
+            }
+
+            return SumOfIntegers(intNumbers);
         }
 
-        private static int SumOfIntegersWithCustomDelimiter(string numbers)
+        private static bool HasNegatives(int[] intNumbers)
+        {
+            foreach (var number in intNumbers)
+            {
+                if (number < 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static int[] GetNegatives(int[] intNumbers)
+        {
+            var listOfNegatives = new List<int>();
+
+            foreach (var number in intNumbers)
+            {
+                if (number < 0)
+                    listOfNegatives.Add(number);
+            }
+
+            return listOfNegatives.ToArray();
+        }
+
+        private static string[] GetValuesWithCustomDelimiter(string numbers)
         {
             var numbersWithoutSlashes = numbers.Substring(2);
             var delimiter = numbersWithoutSlashes.First();
             var values = numbersWithoutSlashes.Split(',', '\n', delimiter);
 
-            return SumOfIntegers(values);
+            return values;
         }
 
-        private static int SumOfIntegers(string[] values)
+        private static int SumOfIntegers(int[] numbers)
         {
             var sum = 0;
 
-            foreach (var value in values)
+            foreach (var number in numbers)
             {
-                if (int.TryParse(value, out var number))
-                {
-                    sum += number;
-                }
+                sum += number;
             }
 
             return sum;
+        }
+
+        private static int[] GetIntegers(string[] numbers)
+        {
+            int[] intNumbers = new int[numbers.Length];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int.TryParse(numbers[i], out intNumbers[i]);
+            }
+
+            return intNumbers;
         }
     }
 }
