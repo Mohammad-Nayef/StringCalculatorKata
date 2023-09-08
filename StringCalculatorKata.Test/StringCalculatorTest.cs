d4247d16a32c13a2d1117bce53a9f4f933dbdd30
mohammad-nayef <awladmohammad20@gmail.com>
2023-09-08 15:23:17 +0300

    Added test for Add method

---
+++ StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace StringCalculatorKata.Test
+{
+    public class StringCalculatorTest
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,1", 2)]
+        public void AddingUpToTwoNumbers(string numbers, int actualSum)
+        {
+            var sum = StringCalculator.Add(numbers);
+
+            sum.Should().Be(actualSum);
+        }
+    }
+}
